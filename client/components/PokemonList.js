67077b5800a652777bd418d7eca687b4e6a64a37
tjhelsel <terence.helsel@gmail.com>
2019-08-15 12:44:43 -0400

    react comments added

---
+++ client/components/PokemonList.js
@@ -0,0 +1,6 @@
+import React, { Component } from 'react';
+
+// Write a component called PokemonList.
+// It should have an array on its state called pokemon.
+// The component should display each of the elements in the
+// array as a separate Pokemon component (already created).
